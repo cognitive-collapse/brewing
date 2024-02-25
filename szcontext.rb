f75757e2624f3589ec9b701f31b0646794362af2
cognitive-collapse <156213371+cognitive-collapse@users.noreply.github.com>
2024-02-25 02:12:27 +0200

    Update szcontext.rb

--- szcontext.rb
+++ szcontext.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Cask-Cookbook
-#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "szcontext" do
   version "1.4"
   sha256 "36d789b863b02a2ba8aa7e50aa248bfd93b478a40e560f33be717da5f1ab4efe"
@@ -10,7 +7,6 @@ cask "szcontext" do
   desc "Functionality for the macOS context menu."
   homepage "https://github.com/RoadToDream/SzContext"
 
-  # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
     url "https://github.com/RoadToDream/SzContext"
     strategy ":github_latest"
@@ -18,5 +14,4 @@ cask "szcontext" do
 
   app "SzContext.app"
 
-  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
 end
